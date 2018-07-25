f4ee498837c17688ceef5fd9be434755545ebd0d
Your Name <you@example.com>
2018-07-25 20:13:42 +0530

    my first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+$(document).ready(() =>{
+        $( ".container1").click(()=>{
+        
+        	$(".container1").hide();
+        	$(".container2").show();
+
+
+        });
+        
+     $( "#button2").click(()=>{
+        
+        	$(".container2").hide();
+        	$(".container1").show();
+
+
+        });    
+       }) ;
